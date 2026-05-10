b5f5710c0854c24d814f272a65d2d648f8fdbe21
mad-alyx <h1101241064@student.untan.ac.id>
2026-05-10 20:58:27 +0700

    rapi rapi

---
+++ apps/frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Ini bakal baca semua file di folder ale, fahdil, dll
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
