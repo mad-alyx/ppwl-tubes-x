03994d2f953a41837bad411d8814ed21e32a2eab
mad-alyx <h1101241064@student.untan.ac.id>
2026-05-10 17:35:00 +0700

    test 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,2 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LogIn_SignIn from './pages/LogIn_SignIn';
-import  from './pages/Beranda';
 
-function App() {
-  return (
-    <Router>
-        <Routes>
-          <Route path="/" element={<Page1 />} />
-          <Route path="/page-2" element={<Page2 />} />
-        </Routes>
-    </Router>
-  );
-}
-export default App;
