4448f256636b26a0fd0ba42512b118cabef85150
mad-alyx <h1101241064@student.untan.ac.id>
2026-05-17 13:05:43 +0700

    perbaikann -2

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,2 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/LogIn_SignIn';
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Halaman utama (/) langsung menampilkan login */}
+        <Route path="/" element={<Login />} />
+        
+        {/* Jika user mengetik url aneh-aneh, paksa balik ke login */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+  );
+}
