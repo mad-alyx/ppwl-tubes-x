6aefa8c4d46aa7cf683317f03c8be1e4948f881b
mad-alyx <h1101241064@student.untan.ac.id>
2026-05-27 19:23:49 +0700

    perbaikan lagi

--- apps/frontend/src/components/rola/Loginmodal.tsx
+++ apps/frontend/src/components/rola/Loginmodal.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { fetchApi } from "../../utils/api";
-import Xlogo from "./Xlogo";
+import Xlogo from "./XLogo";
 
 const isValidEmailFormat = (email: string) => {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
